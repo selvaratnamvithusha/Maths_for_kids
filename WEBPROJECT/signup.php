88bf4b3b66654560d71bcf2a16571ce741e667d5
selvaratnamvithusha <88456626+selvaratnamvithusha@users.noreply.github.com>
2021-12-04 22:25:59 +0530

    Add files via upload

---
+++ WEBPROJECT/signup.php
@@ -0,0 +1,93 @@
+<?php 
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$user_name = $_POST['user_name'];
+		$password = $_POST['password'];
+
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		{
+
+			//save to database
+			$user_id = random_num(20);
+			$query = "insert into users (user_id,user_name,password) values ('$user_id','$user_name','$password')";
+
+			mysqli_query($con, $query);
+
+			header("Location: login.php");
+			die;
+		}else
+		{
+			echo "Please enter some valid information!";
+		}
+	}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Signup</title>
+</head>
+<body>
+
+	<style type="text/css">
+
+    body{
+		background-image: linear-gradient(rgba(0,0,0,0.85),rgba(0,0,0,0.65)),url(s.jpg);
+		height: 100vh;
+		background-size: cover;
+		background-position: center;
+	}	
+		
+	
+    #text{
+
+height: 25px;
+border-radius: 5px;
+padding: 4px;
+border: solid thin #aaa;
+width: 100%;
+}#txt{
+margin-left: 110px;
+}
+#button{
+margin-left: 50px;	
+padding: 10px;
+width: 100px;
+color: white;
+background-color: #4863a0;
+border: none;
+}
+
+#box{
+background-color: #afdcec;
+margin-left: 500px;
+margin-top:200px;
+width: 300px;
+padding: 20px;
+}
+
+	</style>
+
+	<div id="box">
+		
+		<form method="post">
+			<div style="font-size: 20px;margin: 10px;color: white;">Signup</div>
+
+			<input id="text" type="text" name="user_name"><br><br>
+			<input id="text" type="password" name="password"><br><br>
+
+			<input id="button" type="submit" value="Signup"><br><br>
+
+			<a href="login.php">Click to Login</a><br><br>
+		</form>
+	</div>
+</body>
+</html>
